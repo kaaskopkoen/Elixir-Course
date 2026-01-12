38a52f4aabfc269040e1c0fa5109aa04aa2bcaf1
Koen Oosterwijk <koen@oosterwijk.it>
2026-01-12 14:50:13 +0100

    Implemented a whole deck

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,16 @@
 defmodule Cards do
 
-  def hello do
-    "hi there!"
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card)do
+    Enum.member?(deck, card)
   end
 
 end

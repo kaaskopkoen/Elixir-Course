417027d0f454b525336e9c0da9254bfdadeeb1d5
Koen Oosterwijk <koen@oosterwijk.it>
2026-01-13 10:57:27 +0100

    updates

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,8 +2,20 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
 
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %{image | color: {r, g, b}}
+  end
+
+  @spec hash_input(
+          binary()
+          | maybe_improper_list(
+              binary() | maybe_improper_list(any(), binary() | []) | byte(),
+              binary() | []
+            )
+        ) :: %Identicon.Image{color: nil, hex: [byte()]}
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list

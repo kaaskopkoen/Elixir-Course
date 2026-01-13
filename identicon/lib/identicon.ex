b229c7d32daa791642b3228397b697b5afa13d0b
Koen Oosterwijk <koen@oosterwijk.it>
2026-01-13 11:55:29 +0100

    Added grid to the Image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,19 +3,30 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %{image | color: {r, g, b}}
   end
 
-  @spec hash_input(
-          binary()
-          | maybe_improper_list(
-              binary() | maybe_improper_list(any(), binary() | []) | byte(),
-              binary() | []
-            )
-        ) :: %Identicon.Image{color: nil, hex: [byte()]}
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
